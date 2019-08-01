e0d95aa6997e59de0ab978223831df9e771e80bb
Micah Levine <micah.levine@outlook.com>
2019-08-01 12:17:46 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,31 +15,28 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
 
-WIN_COMBINATIONS.each do |win|
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
 
-  win_index_1 = WIN_COMBINATIONS[0]
-  win_index_2 = WIN_COMBINATIONS[1]
-  win_index_3 = WIN_COMBINATIONS[2]
-  win_index_4 = WIN_COMBINATIONS[3]
-  win_index_5 = WIN_COMBINATIONS[4]
-  win_index_6 = WIN_COMBINATIONS[5]
-  win_index_7 = WIN_COMBINATIONS[6]
-  win_index_8 = WIN_COMBINATIONS[7]
+def draw?(board)
+  full?(board) && !won?(board)
+end
 
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
-  position_4 = board[win_index_4]
-  position_5 = board[win_index_5]
-  position_6 = board[win_index_6]
-  position_7 = board[win_index_7]
-  position_8 = board[win_index_8]
+def over?(board)
+  won?(board) || full?(board)
+end
 
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return WIN_COMBINATIONS
-  else
-    false
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
   end
 end
